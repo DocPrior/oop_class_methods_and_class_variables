d4ed33f66d5db2ba8a1678da9ce27b5bd0f24e78
DocPrior <Sara Prior>
2017-05-24 09:39:51 -0400

    start part 3

---
+++ zombie.rb
@@ -0,0 +1,50 @@
+class Zombie
+  @@horde = []
+
+  @@plague_level = 10
+
+  @@max_speed = 5
+
+  @@max_strength = 8
+
+  @@default_speed = 1
+
+  @@default_strength = 3
+
+  attr_accessor :speed :strength
+
+  def initialize(speed, strength)
+    @speed = speed
+    @strength = strength
+    if @speed > @@max_speed
+      @speed = @@default_speed
+    end
+    if @strength >= @@max_strength
+      @strength = @@default_strength
+    end
+  end
+
+  def encounter
+  end
+
+  def outrun_zombie?
+  end
+
+  def surviv_attack?
+  end
+
+  def self.all
+  end
+
+  def self.new_day
+  end
+
+  def self.some_die_off
+  end
+
+  def self.spawn
+  end
+
+  def self.increase_plague_level
+  end
+end

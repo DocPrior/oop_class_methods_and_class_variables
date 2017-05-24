04ca802239d0bc90cc4b3deef972839c49a87150
DocPrior <Sara Prior>
2017-05-24 12:50:42 -0400

    finish methods part 3

--- zombie.rb
+++ zombie.rb
@@ -25,26 +25,66 @@ class Zombie
   end
 
   def encounter
+    outrun_zombie?
+
+    survive_attack?
+
+    if outrun_zombie? == false && survive_attack? == true
+      new_zombie = Zombie.new(rand(@@max_speed), rand(@@strength))
+      @@horde << new_zombie
+      p "You became a zombie."
+    end
+
+    if outrun_zombie? == true
+      p "You escaped"
+    end
+
+    if survive_attack? == false
+      p "You were killed"
+    end
   end
 
   def outrun_zombie?
+    speed = rand(@@max_speed)
+    if speed > @speed 
+      true
+    else
+      false
+    end
   end
 
-  def surviv_attack?
+  def survive_attack?
+    strength = rand(@@strength)
+    if strength > @strength
+      true
+    else
+      false
+    end
   end
 
   def self.all
+    @@horde
   end
 
   def self.new_day
+    Zombie.some_die_off
+    Zombie.spawn
+    Zombie.increase_plague_level
   end
 
   def self.some_die_off
+    @@horde.drop(rand(@@plague_level))
   end
 
   def self.spawn
+    rand(@@plague_level).times do
+      new_zombie = Zombie.new(rand(@@max_speed), rand(@@strength))
+      @@horde << new_zombie
+    end
+    @@horde
   end
 
   def self.increase_plague_level
+    @@plague_level += rand(3)
   end
 end
